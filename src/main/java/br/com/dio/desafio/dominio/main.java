851dcc98002eba8e1c92cb0c5e03e13342ec2706
robsonrsc <robsonrsc.stos@gmail.com>
2023-01-13 13:53:47 -0300

    Desafio

---
+++ src/main/java/br/com/dio/desafio/dominio/main.java
@@ -0,0 +1,25 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class main {
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        System.out.println("\n\n");
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Conceitos basicos de Java");
+        curso.setCargahoraria(5);
+
+        System.out.println(curso);
+
+
+        mentoria.setTitulo("Java");
+        mentoria.setDescricao("Ensino de java basico ao avancado");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
